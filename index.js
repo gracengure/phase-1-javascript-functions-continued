a169e440087eb6a801bae111bcf0479de0e2a6be
grace ngure <wnguregrace@gmail.com>
2024-04-01 14:54:09 +0300

    add index.js

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
-// code your solution here
+function saturdayFun(game="roller-skate") {
+    return `This Saturday, I want to ${game}!`
+  }
+
+let mondayWork= function (work="go to the office") {
+    return `This Monday, I will ${work}.`
+  }
+
+  let wrapAdjective = function(wrap="*") {
+    return function(adjective="special") {
+      return `You are ${wrap}${adjective}${wrap}!`
+    }
+  }
+    
